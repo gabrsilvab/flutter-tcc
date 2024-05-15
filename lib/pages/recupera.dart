f40becc41ce4ee69758e343b385bfcc830932dd7
olyvia01062007 <117852627+olyvia01062007@users.noreply.github.com>
2024-05-15 15:13:30 -0300

    Update recupera.dart

--- lib/pages/recupera.dart
+++ lib/pages/recupera.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:modulo01/controllers/recupera.dart';
 
+//CRIA CLASSES IMPORTANTES
 class RecuperarSenhaPage extends StatefulWidget {
   @override
   _RecuperarSenhaPageState createState() => _RecuperarSenhaPageState();
@@ -18,6 +19,7 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
+    //ELE COLOCA A IMAGEM DO "MENU-LOGIN"
     return Scaffold(
       backgroundColor: Colors.black,
       body: Stack(
@@ -31,6 +33,7 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
               fit: BoxFit.cover,
             ),
           ),
+          //ESCREVE O E-MAIL PARA O USUÁRIO RECEBER O CÓDIGO DE "ESQUECEU SENHA"
           Padding(
             padding: const EdgeInsets.all(20),
             child: SizedBox(
@@ -73,6 +76,7 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
                                 ),
                                 backgroundColor: Colors.white,
                               ),
+                              //TEXTO "ENVIAR E-MAIL DE RECUPERAÇÃO
                               child: Text(
                                 'Enviar E-mail de Recuperação',
                                 style: TextStyle(
@@ -87,6 +91,7 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
                       : Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
+                            //TEXTO
                             TextField(
                               controller: _codigoController,
                               enabled: true,
@@ -105,10 +110,12 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
                                 color: Colors.white,
                               ),
                             ),
+                            //VALIDAR O CÓDIGO
                             const SizedBox(height: 20),
                             ElevatedButton(
                               onPressed: () {
                                 _validarCodigo();
+                              //CAMPO DE ESCRITA DO CÓDIGO DE RECUPERAÇÃO DE SENHA
                               },
                               style: ElevatedButton.styleFrom(
                                 fixedSize: Size(width, 40),
@@ -117,6 +124,7 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
                                 ),
                                 backgroundColor: Colors.white,
                               ),
+                              //BOTÃO DE "ENVIAR"
                               child: Text(
                                 'Enviar',
                                 style: TextStyle(
@@ -128,6 +136,7 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
                             ),
                           ],
                         ),
+                  //QUANDO O CÓDIGO FOR ENVIADO, O USUÁRIO RECEBERÁ UMA NOTIFICAÇÃO AVISANDO QUE O PROCESSO FOI FEITO
                   const SizedBox(height: 10),
                   Text(
                     _mensagem,
@@ -145,6 +154,7 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
     );
   }
 
+  //VERIFICA SE O CAMPO "E-MAIL" NÃO ESTÁ VAZIO
   void _enviarEmail() async {
     String email = _emailController.text;
     bool enviadoComSucesso = await _controller.enviarEmailRecuperacao(email);
@@ -161,11 +171,13 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
     }
   }
 
+  //SIMULAÇÃO COM O CÓDIGO PADRÃO
   void _validarCodigo() {
     String codigo = _codigoController.text;
 
     if (codigo == '111111') {
       // Show dialog before redirection
+      //AVISA QUE A SENHA FOI TROCADA
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -180,6 +192,7 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
               style: TextStyle(color: Colors.white),
             ),
             actions: [
+              //BOTÃO QUE FARÁ O USUÁRIO VOLTAR A TELA DE LOGIN
               TextButton(
                 onPressed: () {
                   Navigator.of(context).pop();
@@ -194,6 +207,7 @@ class _RecuperarSenhaPageState extends State<RecuperarSenhaPage> {
           );
         },
       );
+    //SE O CÓDIGO ESTIVER ERRADO ELE IRÁ DAR UM ALERTA
     } else {
       setState(() {
         _mensagem = 'Código incorreto. Por favor, tente novamente.';
