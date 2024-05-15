f60b3a91fac536af8cc334ee0705c6ad068cd28c
Gabriel da Silva <133811804+gabrsilvab@users.noreply.github.com>
2024-05-15 14:32:36 -0300

    Update splash.dart

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -16,7 +16,7 @@ class _SplashState extends State<Splash> {
       (value) async {
         CacheController.logar();
         await Future.delayed(Duration(seconds: value[0]));
-        Navigator.pushNamed(context, '/home');
+        Navigator.pushNamed(context, '/login');
       },
     );
   }
